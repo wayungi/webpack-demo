3b956a4a431bb6d3e4222afbc7c317298fc8799a
Francis <wayncis2@gmail.com>
2022-05-10 23:52:34 +0300

    Add entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
