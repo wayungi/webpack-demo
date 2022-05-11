3317ed60b98380973ea96bec115af7e45828a66f
Francis <wayncis2@gmail.com>
2022-05-11 22:04:32 +0300

    Add loading data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import _ from 'lodash';
 import printMe from './print.js';
 import './style.css';
 import Icon from './africa.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
@@ -19,6 +33,9 @@ function component() {
   element.appendChild(myIcon);
 
   element.appendChild(btn);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
